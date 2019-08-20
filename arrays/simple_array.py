ddde4b829df1219e8f1b595b2f9ad864e78087bd
lihao <imagelihao@163.com>
2019-08-20 21:57:56 +0800

    简单的构造杨辉三角

--- arrays/simple_array.py
+++ arrays/simple_array.py
@@ -99,7 +99,85 @@ class Solution(object):
                 left = mid + 1
         return left
 
+    @classmethod
+    def maxSubArrayForce(cls,nums):
+        """
+        求最大子序列
+        TODO : 未完待写
+        :param nums:
+        :return:
+        """
+        maxSum = nums[0]
+        tmp = maxSum
+        length = len(nums)
+        for i in range(1,length):
+            if tmp+nums[i] > tmp:
+                maxSum = max(maxSum,tmp+nums[i])
+                tmp = tmp + nums[i]
+            else:
+                maxSum = max(maxSum,nums[i] + tmp,nums[i])
+                tmp = nums[i]
+        return maxSum
 
+    @classmethod
+    def plusOne(cls,digits):
+        """
+        对一个数组加1
+        :param digits:
+        :return:
+        """
+        add = 1
+        for i in range(-1,(len(digits)+1)*-1,-1):
+            if add+digits[i] > 9:
+                digits[i] = 0
+            else:
+                digits[i] = digits[i]+1
+                break
+        if digits[0] == 0:
+            digits.insert(0,1)
+        return digits
 
-nums = [1]
-print(Solution.searchInsert(nums,7))
+    @classmethod
+    def merge(cls,nums1,m,nums2,n):
+        """
+        给定两个有序整数数组 nums1 和 nums2，将 nums2 合并到 nums1 中，使得 num1 成为一个有序数组。
+        初始化 nums1 和 nums2 的元素数量分别为 m 和 n。
+        你可以假设 nums1 有足够的空间（空间大小大于或等于 m + n）来保存 nums2 中的元素。
+        :param nums1: 
+        :param m: 
+        :param nums2: 
+        :param n: 
+        :return: 
+        """
+        k = m+n-1
+        while k>0 and m>0 and n>0:
+            if nums1[m-1] > nums2[n-1]:
+                nums1[k] = nums1[m-1]
+                m = m - 1
+            else:
+                nums1[k] = nums2[n-1]
+                n = n - 1
+            k = k - 1
+        if n > 0:
+            nums1[:n] = nums2[:n]
+
+    @classmethod
+    def generate(cls,numRows):
+        """
+        给定一个非负整数 numRows，生成杨辉三角的前 numRows 行。
+        :param numRows:
+        :return:
+        """
+        if numRows<=0:
+            return [[]]
+        t = [[1]]
+        if numRows==1:
+            return t
+        for i in range(2,numRows+1):
+            c= [0 for j in range(i)]
+            c[0] =1
+            c[-1] =1
+            for j in range(1,len(c)-1):
+                c[j] = t[i-2][j-1] + t[i-2][j]
+            t.append(c)
+        return t
